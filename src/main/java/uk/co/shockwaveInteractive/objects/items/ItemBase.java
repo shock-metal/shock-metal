fc3f0345968885b2dd92d31f0f9386b54b842732
HarmanU <uppalhrs@aston.ac.uk>
2018-03-14 14:38:24 +0000

    Added parameter to set hasEffect boolean

--- src/main/java/uk/co/shockwaveInteractive/objects/items/ItemBase.java
+++ src/main/java/uk/co/shockwaveInteractive/objects/items/ItemBase.java
@@ -1,18 +1,23 @@
 package uk.co.shockwaveInteractive.objects.items;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import uk.co.shockwaveInteractive.ShockMetalMain;
 import uk.co.shockwaveInteractive.init.ItemInit;
 import uk.co.shockwaveInteractive.util.interfaces.IHasModel;
 
 public class ItemBase extends Item implements IHasModel
 {
-	public ItemBase(String name)
+	public boolean isRare;
+	
+	public ItemBase(String name, boolean isRare)
 	{
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(ShockMetalMain.shockmetaltab);
+		this.isRare = isRare;
 		
 		ItemInit.ITEMS.add(this);
 	}
@@ -22,6 +27,17 @@ public class ItemBase extends Item implements IHasModel
 		
 		ShockMetalMain.proxy.registerItemRenderer(this, 0, "inventory");
 		
+	}
+	
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		
+		if(isRare)
+		{
+			return true;
+		}
+		
+		return super.hasEffect(stack);
 	}
 	
 }
