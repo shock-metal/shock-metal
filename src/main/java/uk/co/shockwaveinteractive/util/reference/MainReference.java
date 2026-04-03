ddada5d18e52df876d0ce2a76c484c56a6f562a2
vNemesis <harman.uppal@shockwaveinteractive.co.uk>
2026-04-03 02:45:13 +0100

    refactor: remove legacy uk.co.shockwaveInteractive codebase

--- src/main/java/uk/co/shockwaveinteractive/util/reference/MainReference.java
+++
@@ -1,8 +0,0 @@
-package uk.co.shockwaveinteractive.util.reference;
-
-public class MainReference {
-
-	public static final String MODID = "shockmetal";
-	public static final String TRANSLATION_INFO_PREFIX = "info.shockmetal.gui.";
-	public static final String TRANSLATION_SHIFT_INFO = "info.shockmetal.gui.shift-info";
-}
